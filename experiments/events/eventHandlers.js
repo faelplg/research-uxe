0a55fd7454c2fc2ef76471ede5fa6e0c35e9470a
Rafael Goulart <rafaelgoulart@192.168.0.15>
2025-06-04 02:26:54 -0300

    updating palette

--- experiments/events/eventHandlers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export function handleButtonClick(event) {
-  console.log('EVENT: Handle button click triggered!', event);
-}
-
-export function handleEvent(data) {
-  console.log('EVENT: Handle event triggered', data);
-}
